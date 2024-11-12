c9c3685c1374621bda09681ccbf8891d20508727
IrinaMuravyova <miomir@yandex.ru>
2024-11-12 11:58:42 +0800

    Added icon to Profile model

--- CalorieCalculation/Models/Profile.swift
+++ CalorieCalculation/Models/Profile.swift
@@ -5,4 +5,33 @@
 //  Created by Irina Muravyeva on 12.11.2024.
 //
 
-import Foundation
+struct Profile {
+    let person: String
+    let icon: String
+    let sex: Sex
+    let height: Double
+    let weight: Double
+    let activityLevel: ActivityLevel
+    let goal: Goals
+    let caloriesBMT: Int?
+    let caloriesTDEEForGoal: [ActivityLevel: Int]?
+    
+}
+
+enum Sex {
+    case male
+    case female
+}
+
+enum Goals {
+    case weightLoss
+    case savingWeight
+    case weightGain
+}
+enum ActivityLevel: Double {
+    case sedentary = 1.2
+    case lightActivity = 1.375
+    case normalActivity = 1.55
+    case highActivity = 1.725
+    case extremalActivity = 1.9
+}

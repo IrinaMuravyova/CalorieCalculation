ca9bd4ddc5c65521834b7b07f03136158627697a
IrinaMuravyova <miomir@yandex.ru>
2024-11-20 15:23:35 +0800

    Added text about calculation when questionButtonTapped

---
+++ CalorieCalculation/ViewControllers/HelpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HelpViewController.swift
+//  CalorieCalculation
+//
+//  Created by Irina Muravyeva on 20.11.2024.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
